54c1b0aef432d0f3ddcf949bd1fe28ba15c958d6
Thorsten von Eicken <tve@voneicken.com>
2020-04-16 00:01:18 -0700

    start tests by loading firmware

--- gohci.sh
+++ gohci.sh
@@ -4,5 +4,5 @@ export PATH=$HOME/bin:$PATH
 
 echo "----- flashing MicroPython -----"
 src=$HOME/save/mp/tve/esp32/firmware.bin
-./esp-idf-v4/components/esptool_py/esptool/esptool.py --chip esp32 --port $PORT --baud 460800 \
+$HOME/esp-idf-v4/components/esptool_py/esptool/esptool.py --chip esp32 --port $PORT --baud 460800 \
     write_flash -z --flash_mode dio --flash_freq 40m 0x1000 $src
