31ec53ea49b7802106f87589d9dd6392e018b43d
Thorsten von Eicken <tve@voneicken.com>
2020-05-25 18:29:13 -0700

    switch load.sh from pyboard to pyboard.py

--- load.sh
+++ load.sh
@@ -2,17 +2,17 @@
 DIR=$(dirname $0)
 echo device: ${PYBOARD_DEVICE:-/dev/ttyACM0}
 
-pyboard -f cp $DIR/board/boot.py :
-[[ "$(pyboard -f ls)" == *safemode* ]] || pyboard -f mkdir /safemode
-pyboard -f cp \
+pyboard.py -f cp $DIR/board/boot.py :
+[[ "$(pyboard.py -f ls)" == *safemode* ]] || pyboard.py -f mkdir /safemode
+pyboard.py -f cp \
     $DIR/board/{main,board,logging,mqtt}.py \
     $DIR/mqrepl/{mqrepl,watchdog}.py \
     $DIR/mqtt_async/mqtt_async.py \
     :/safemode/
 
-if [[ "$(pyboard -f ls)" != *board_config.py* ]]; then
+if [[ "$(pyboard.py -f ls)" != *board_config.py* ]]; then
     if [[ -f $DIR/board/board_config.py ]]; then
-        pyboard -f cp $DIR/board/board_config.py :/safemode/
+        pyboard.py -f cp $DIR/board/board_config.py :/safemode/
     else
         echo "Please load board_config.py manually"
     fi
